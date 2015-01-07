45e6d1f0014cb9450681ac912982d20169b1e0f2
Gleb Popov <glebche@gmail.com>
2015-01-06 22:15:08 -0500

    updated donation page & NG controller/service logic

--- ui/app/homepage/js/controllers.js
+++ ui/app/homepage/js/controllers.js
@@ -41,10 +41,37 @@ homepageControllers.controller('EventsCtrl', ['$scope',
 /**
 * Donation Controller
 */
-homepageControllers.controller('DonationCtrl', ['$scope', 
-	function($scope) {
+homepageControllers.controller('DonationCtrl', ['$scope', 'DonationService',
+	function($scope, DonationService) {
 		window.log('DonationCtrl: init..');
+
+		//init page model
+		$scope.donationModel = {}; 
 		
+		//triggered on the 'Donation Now' button click
+		$scope.proceedToDonate = function()
+		{ 
+			window.app_log('controller', 'DonationCtrl: submitting data..');
+
+			DonationService.submitData($scope.donationModel).then(function(response) {
+				if (response && response.status == 200 && response.data.Payload &&
+					response.data && response.data.Status == 'Success'  && response.data.Payload.token
+					)
+		    	{
+		    		//set quick status for the next controller
+					SharedDataService.setData('Successfully submitted data.');
+
+					//redirect back to the list page
+					$location.path('/donation/thank-you');
+				 	return;
+
+		    	} else
+		    	{
+		    		$scope.error_message = 'Unable to update the record. Please contact support.';
+
+		    	}
+		    });
+		}
 	}
 ]);
 
--- ui/app/homepage/js/directives.js
+++ ui/app/homepage/js/directives.js
@@ -8,6 +8,26 @@ define(['angular'], function (angular) {
 
 "use strict";
 
+var globalDirectives = angular.module('globalDirectives', []);
+
+/**
+* Google Maps Route Distance Directive
+* Usage: <div google-maps-route-distance class="well"> </div>
+*/
+globalDirectives.directive("googleMapsRouteDistance", function() {
+    window.app_log('directive', 'googleMapsRouteDistance: setting up the \'google-maps-route-distance\' directive');
+    return {
+        restrict : "A",
+        template: " ",
+        link : function(scope, element, attrs) {
+        	var directionsDisplay;
+			var directionsService = new google.maps.DirectionsService();
+			var map;
+            element.html("HELLO");
+        }
+    };
+});
+
 
     
 });
