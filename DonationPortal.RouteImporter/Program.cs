d493da0ad8a8e20170ad1c5852b8db31a1449efc
Kevin Gregory <medkg15@gmail.com>
2015-01-13 14:31:17 -0500

    create an importer for routes

---
+++ DonationPortal.RouteImporter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonationPortal.RouteImporter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//var import = new CsvRouteImporter(2);
+			//import.Import(@"C:\github\3KMilesCure\DonationPortal.RouteImporter\Samples\sebring_short.csv");
+
+			//var import = new CsvRouteImporter(1);
+			//import.Import(@"C:\github\3KMilesCure\DonationPortal.RouteImporter\Samples\sebring_racetrack.csv");
+
+			var import = new CsvRouteImporter(3);
+			import.Import(@"C:\github\3KMilesCure\DonationPortal.RouteImporter\Samples\sebring_long.csv");
+		}
+	}
+}
