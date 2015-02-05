0884d373fc86eeb4fffe0523511842222d071b09
Kevin Gregory <medkg15@gmail.com>
2015-02-05 14:49:39 -0500

    fix donation map display

--- DonationPortal.Web/ui/scripts/riderDonate.js
+++ DonationPortal.Web/ui/scripts/riderDonate.js
@@ -17,7 +17,7 @@
     				mapTypeId: google.maps.MapTypeId.ROADMAP
     			};
 
-    			var map = new google.maps.Map(document.getElementById('map-canvas'),
+    			var map = new google.maps.Map(document.getElementById('donation-map'),
                     mapOptions);
 
     			var marker = new google.maps.Marker({
