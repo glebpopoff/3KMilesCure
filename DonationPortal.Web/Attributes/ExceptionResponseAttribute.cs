b6a89dbe335a500fd0fe4c0d5fc536e664e361e1
Kevin Gregory <medkg15@gmail.com>
2015-02-11 17:26:05 -0500

    global error handling for asp.net (mvc) and web api

---
+++ DonationPortal.Web/Attributes/ExceptionResponseAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Filters;
+using log4net;
+
+namespace DonationPortal.Web.Attributes
+{
+	public class ExceptionResponseAttribute : ExceptionFilterAttribute
+	{
+		private static readonly ILog _log = LogManager.GetLogger(typeof(ExceptionResponseAttribute));
+
+		public override void OnException(HttpActionExecutedContext actionExecutedContext)
+		{
+			var actionName = actionExecutedContext.ActionContext.ActionDescriptor.ActionName;
+			var controllerName = actionExecutedContext.ActionContext.ControllerContext.ControllerDescriptor.ControllerType.FullName;
+
+			_log.Error(
+				string.Format(
+					"Unhandled exception processing action {0} for controller {1}.",
+					actionName,
+					controllerName),
+				actionExecutedContext.Exception);
+
+			base.OnException(actionExecutedContext);
+		}
+	}
+}
