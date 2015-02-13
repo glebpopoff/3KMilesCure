3c832afe4177407518fcdcd7b49e8cd45b7c7dab
Kevin Gregory <medkg15@gmail.com>
2015-02-13 16:44:40 -0500

    404 error when donating for a rider that doesn't exist.

--- DonationPortal.Web/Controllers/API/EventDonationController.cs
+++ DonationPortal.Web/Controllers/API/EventDonationController.cs
@@ -8,13 +8,15 @@ using DonationPortal.Engine;
 using DonationPortal.Engine.PaymentProcessor;
 using DonationPortal.Web.ApiModels.EventDonations;
 using DonationPortal.Web.Attributes;
+using log4net;
 
 namespace DonationPortal.Web.Controllers.API
 {
 	[ConditionalRequireHttps]
 	[RoutePrefix("api/v1")]
     public class EventDonationController : ApiController
-    {
+	{
+		private static readonly ILog _log = LogManager.GetLogger(typeof (EventDonationController));
 		private readonly IImmediatePaymentProcessor _immediatePaymentProcessor;
 
 		public EventDonationController()
@@ -28,30 +30,44 @@ namespace DonationPortal.Web.Controllers.API
 		{
 			if (!ModelState.IsValid)
 			{
+				_log.DebugFormat("Invalid request to donate to event rider {0} {1}.", eventSlug, riderSlug);
+
 				return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
 			}
 
-			// 404 or 400's for events/riders that don't exist?
-
-			var paymentResult = _immediatePaymentProcessor.Process(new ImmediatePaymentRequest
-			{
-				Amount = donation.DonationAmount,
-				City = donation.City,
-				CvvNumber = donation.CvvNumber,
-				Email = donation.Email,
-				ExpirationMonth = donation.ExpirationMonth,
-				ExpirationYear = donation.ExpirationYear,
-				FirstName = donation.FirstName,
-				LastName = donation.LastName,
-				State = donation.State,
-				StreetAddress1 = donation.StreetAddress1,
-				StreetAddress2 = donation.StreetAddress2,
-				ZipCode = donation.ZipCode,
-				CreditCardNumber = donation.CreditCardNumber
-			});
-
 			using (var entities = new DonationPortalEntities())
 			{
+				var @event = entities.Events.SingleOrDefault(e => e.UrlSlug.Equals(eventSlug));
+
+				if (@event == null)
+				{
+					return Request.CreateErrorResponse(HttpStatusCode.NotFound, string.Format("Event {0} could not be found.", eventSlug));
+				}
+
+				var rider = @event.EventRiders.SingleOrDefault(r => r.UrlSlug.Equals(riderSlug));
+
+				if (rider == null)
+				{
+					return Request.CreateErrorResponse(HttpStatusCode.NotFound, string.Format("Rider {0} could not be found.", riderSlug));
+				}
+
+				var paymentResult = _immediatePaymentProcessor.Process(new ImmediatePaymentRequest
+				{
+					Amount = donation.DonationAmount,
+					City = donation.City,
+					CvvNumber = donation.CvvNumber,
+					Email = donation.Email,
+					ExpirationMonth = donation.ExpirationMonth,
+					ExpirationYear = donation.ExpirationYear,
+					FirstName = donation.FirstName,
+					LastName = donation.LastName,
+					State = donation.State,
+					StreetAddress1 = donation.StreetAddress1,
+					StreetAddress2 = donation.StreetAddress2,
+					ZipCode = donation.ZipCode,
+					CreditCardNumber = donation.CreditCardNumber
+				});
+
 				entities.RiderMessageDonations.Add(new RiderMessageDonation
 				{
 					City = donation.City,
@@ -67,7 +83,7 @@ namespace DonationPortal.Web.Controllers.API
 					TransactionID = paymentResult.TransactionID,
 					PaymentResource = paymentResult.PaymentResource,
 					ZipCode = donation.ZipCode,
-					EventRider = entities.Events.Single(e => e.UrlSlug.Equals(eventSlug)).EventRiders.Single(r => r.UrlSlug.Equals(riderSlug)),
+					EventRider = rider,
 					Amount = donation.DonationAmount,
 					Date = DateTime.Now
 				});
